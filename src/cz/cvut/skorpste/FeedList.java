a3a6198773f4f43a4b90cc335d8456e84e01c9c4
Štěpán Škorpil <stepan.s@gmail.com>
2014-03-13 13:32:41 +0100

    Initial commit

---
+++ src/cz/cvut/skorpste/FeedList.java
@@ -0,0 +1,15 @@
+package cz.cvut.skorpste;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FeedList extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
