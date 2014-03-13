02feefc48d8a5054245c41da72191a9c090c915f
Štěpán Škorpil <stepan.s@gmail.com>
2014-03-14 00:23:32 +0100

    Klikatelný list

---
+++ src/cz/cvut/skorpste/ArticleArrayAdapter.java
@@ -0,0 +1,33 @@
+package cz.cvut.skorpste;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by stopka on 13.3.14.
+ */
+public class ArticleArrayAdapter extends ArrayAdapter {
+
+
+    public ArticleArrayAdapter(Context context, int resource, Object[] objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.listitem, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.title);
+        String title=(String)getItem(position);
+        textView.setText(title);
+
+        return rowView;
+    }
+
+
+}
--- src/cz/cvut/skorpste/ListFragment.java
+++ src/cz/cvut/skorpste/ListFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by stopka on 13.3.14.
@@ -12,6 +18,28 @@ import android.view.ViewGroup;
 public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.listfragment, container, false);
+        View view = inflater.inflate(R.layout.listfragment, container, false);
+
+        ListView articleList = (ListView) view.findViewById(R.id.list);
+        String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile"};
+        ArrayAdapter<String> adapter = new ArticleArrayAdapter(this.getActivity(), R.layout.listitem, values);
+        articleList.setAdapter(adapter);
+        articleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                Toast.makeText(parent.getContext(),
+                        "Click ListItem Number " + item, Toast.LENGTH_LONG)
+                        .show();
+            }
+
+        });
+        return view;
     }
 }
