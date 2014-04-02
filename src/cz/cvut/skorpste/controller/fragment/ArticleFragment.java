0650b03dbdc1aeccafe2851191d068fb967835cb
Štěpán Škorpil <stepan.s@gmail.com>
2014-04-02 15:35:20 +0200

    Oprava ikon, Oprava share tlačítka, Oprava Article  detailu

--- src/cz/cvut/skorpste/controller/fragment/ArticleFragment.java
+++ src/cz/cvut/skorpste/controller/fragment/ArticleFragment.java
@@ -1,7 +1,12 @@
 package cz.cvut.skorpste.controller.fragment;
 
 import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.ShareActionProvider;
@@ -9,54 +14,109 @@ import android.widget.TextView;
 import cz.cvut.skorpste.model.Article;
 import cz.cvut.skorpste.model.Articles;
 import cz.cvut.skorpste.R;
+import cz.cvut.skorpste.model.database.ArticleContentProvider;
+import cz.cvut.skorpste.model.database.ArticleTable;
+
+import java.util.Date;
 
 /**
  * Created by stopka on 13.3.14.
  */
-public class ArticleFragment extends Fragment {
+public class ArticleFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     public static final String ARTICLE = "article";
     public static final String ARTICLE_ID = "article_id";
 
-    private Article article;
+    private final int ARTICLE_LOADER = 1;
+
+    String shareLink=null;
+    String shareTitle=null;
 
     public ArticleFragment() {
-
+        super();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        long id=getArguments().getLong(ARTICLE_ID);
-        this.article = Articles.get().getFeed(id);
         setHasOptionsMenu(true);
+        getLoaderManager().initLoader(ARTICLE_LOADER, getArguments(), this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.article_fragment, container, false);
-        TextView title=(TextView)view.findViewById(R.id.title);
-        title.setText(article.getTitle());
-        TextView author=(TextView)view.findViewById(R.id.author);
-        author.setText(article.getAuthor());
-        TextView article=(TextView)view.findViewById(R.id.text);
-        article.setText(this.article.getText());
-        TextView date=(TextView)view.findViewById(R.id.date);
-        date.setText(this.article.getDate().toString());
-
-        return view;
+        return inflater.inflate(R.layout.article_fragment, container, false);
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.article, menu);
-        MenuItem item = menu.findItem(R.id.menu_item_share);
+        /*if (shareLink == null) {
+            menu.findItem(R.id.menu_article_share).setEnabled(false);
+        }*/
+    }
 
-        ShareActionProvider shareActionProvider = (ShareActionProvider) menu.findItem(R.id.menu_item_share).getActionProvider();
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_SUBJECT, R.string.share_subject);
-        intent.putExtra(Intent.EXTRA_TEXT, article.getTitle());
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case ARTICLE_LOADER:
+                getActivity().setProgressBarIndeterminate(true);
+                getActivity().setProgressBarVisibility(true);
+                long article_id = args.getLong(ARTICLE_ID);
+                CursorLoader cursorLoader = new CursorLoader(getActivity(), ContentUris.withAppendedId(ArticleContentProvider.ARTICLE_URI, article_id), null, null, null, null);
+                return cursorLoader;
+            default:
+                return null;
+        }
 
-        shareActionProvider.setShareIntent(intent);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        switch (loader.getId()) {
+            case ARTICLE_LOADER:
+                data.moveToFirst();
+                shareLink=data.getString(data.getColumnIndex(ArticleTable.LINK));
+                shareTitle=data.getString(data.getColumnIndex(ArticleTable.TITLE));
+                TextView title = (TextView) getView().findViewById(R.id.title);
+                title.setText(shareTitle);
+                TextView author = (TextView) getView().findViewById(R.id.author);
+                author.setText(data.getString(data.getColumnIndex(ArticleTable.AUTHOR)));
+                TextView article = (TextView) getView().findViewById(R.id.text);
+                article.setText(data.getString(data.getColumnIndex(ArticleTable.CONTENT)));
+                TextView date = (TextView) getView().findViewById(R.id.date);
+                Date date_value = new Date(data.getLong(data.getColumnIndex(ArticleTable.DATE)));
+                date.setText(date_value.toString());
+
+                getActivity().setProgressBarVisibility(false);
+                break;
+        }
+
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        switch (loader.getId()) {
+            case ARTICLE_LOADER:
+
+                getActivity().setProgressBarVisibility(true);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_article_share:
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.share_subject, shareTitle));
+                shareIntent.putExtra(Intent.EXTRA_TEXT,getString(R.string.share_text, shareLink));
+                Intent chooser = Intent.createChooser(shareIntent, getString(R.string.share_chooser));
+                startActivity(chooser);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
